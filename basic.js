d6653c1f0905b4518c296b13e96decfb1d496f0b
Nitish8800 <nitishfea120784@gmail.com>
2022-06-24 01:31:41 +0530

    files folder make

--- basic.js
+++ basic.js
@@ -60,6 +60,12 @@ const path = require("path");
 const dirPath = path.join(__dirname, "files");
 // console.log(dirPath)
 
-for (var i = 1; i <= 5; i++) {
-  fs.writeFileSync(dirPath + `/hello${i}.txt`, `A simple text file ${i}`);
-}
+// for (var i = 1; i <= 5; i++) {
+//   fs.writeFileSync(dirPath + `/hello${i}.txt`, `A simple text file ${i}`);
+// }
+
+fs.readdir(dirPath, (err, files) => {
+  files.forEach((el) => {
+    console.log(el);
+  });
+});
