07252b1b2ec356428575d157de85202c1e8fbd29
Nitish8800 <nitishfea120784@gmail.com>
2022-06-24 00:13:52 +0530

    http core modules create server

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-// Create new file by code in nodejs fs
+//{ 1.} // Create new file by code in nodejs fs
 
-const fs = require("fs");
-// console.log("code step by step");
+// const fs = require("fs");
+// // console.log("code step by step");
+// fs.writeFileSync("app.txt", "NiKU");
 
-fs.writeFileSync("app.txt", "NiKU");
+// {2.} // for checking directory
+// console.log("<<...>>", __dirname);
+// console.log("<<...>>", __filename);
 
-// for checking directory
 
-console.log("<<...>>", __dirname);
-console.log("<<...>>", __filename);
+
+
+// {3.} HTTP core modules
+
+const http = require('http')
+
+http.createServer((req,res)=>{
+
+
+
+
+  
+}).listen(8080)
+
+
+
+
