a96a917bcfe35c5e8a15dc60839b1613496f7f78
Nitish8800 <nitishfea120784@gmail.com>
2022-06-24 01:19:48 +0530

    Display file list from folder

--- basic.js
+++ basic.js
@@ -39,16 +39,23 @@
 //   })
 //   .listen(5000);
 
-// /<<<<>>>>>>>>>>{ {7.} Getting input from command line
+// // /<<<<>>>>>>>>>>{ {7.} Getting input from command line
 
-const fs = require("fs");
+// const fs = require("fs");
 
-const input = process.argv;
+// const input = process.argv;
 
-if (input[2] == "add") {
-  fs.writeFileSync(input[2], input[3]);
-} else if (input[2] == "remove") {
-  fs.unlinkSync(input[3]);
-} else {
-  console.log("Invalid Output");
-}
+// if (input[2] == "add") {
+//   fs.writeFileSync(input[3], input[4]);
+// } else if (input[2] == "remove") {
+//   fs.unlinkSync(input[3]);
+// } else {
+//   console.log("Invalid Output");
+// }
+
+
+// // /<<<<>>>>>>>>>>{ {7.} Display file list from folder
+
+
+
+
