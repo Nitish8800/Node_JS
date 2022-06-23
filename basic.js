01fdf2c6ea0c9ba27dc7264fd72f0ffcae3a49d9
Nitish8800 <nitishfea120784@gmail.com>
2022-06-24 00:54:10 +0530

    basic api

---
+++ basic.js
@@ -0,0 +1,38 @@
+//<<<<>>>>>>>>>>{ 1.} // Create new file by code in nodejs fs
+
+// const fs = require("fs");
+// // console.log("code step by step");
+// fs.writeFileSync("app.txt", "NiKU");
+
+////<<<<>>>>>>>>>>{ {2.} // for checking directory
+// console.log("<<...>>", __dirname);
+// console.log("<<...>>", __filename);
+
+// //<<<<>>>>>>>>>>{ {3.} HTTP core modules {Create Server}
+
+// const http = require("http");
+
+// http
+//   .createServer((req, res) => {
+//     res.write("<h1>This is my world</h1>");
+//     res.end();
+//   })
+//   .listen(8080);
+
+//<<<<>>>>>>>>>>{ {5.} Colors use
+
+// const colors = require("colors");
+
+// console.log("Hello".red);
+// console.warn("NIKU".blue);
+
+//<<<<>>>>>>>>>>{ {6.} Simple and basic API
+
+const http = require("http");
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "applicationjson" });
+    res.write(JSON.stringify({ name: "Nitish Kumar", email: "nk@test" }));
+    res.end();
+  })
+  .listen(5000);
