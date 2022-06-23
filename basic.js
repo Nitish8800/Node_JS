c06e5020d1aa316d5408701a7dd903afe37d1ad1
Nitish8800 <nitishfea120784@gmail.com>
2022-06-24 01:51:20 +0530

    Crud With file System

--- basic.js
+++ basic.js
@@ -53,19 +53,51 @@
 //   console.log("Invalid Output");
 // }
 
-// // /<<<<>>>>>>>>>>{ {7.} Display file list from folder
+// // // /<<<<>>>>>>>>>>{ {7.} Display file list from folder
 
-const fs = require("fs");
-const path = require("path");
-const dirPath = path.join(__dirname, "files");
-// console.log(dirPath)
+// const fs = require("fs");
+// const path = require("path");
+// const dirPath = path.join(__dirname, "files");
+// // console.log(dirPath)
 
-// for (var i = 1; i <= 5; i++) {
-//   fs.writeFileSync(dirPath + `/hello${i}.txt`, `A simple text file ${i}`);
-// }
+// // for (var i = 1; i <= 5; i++) {
+// //   fs.writeFileSync(dirPath + `/hello${i}.txt`, `A simple text file ${i}`);
+// // }
 
-fs.readdir(dirPath, (err, files) => {
-  files.forEach((el) => {
-    console.log(el);
-  });
-});
+// fs.readdir(dirPath, (err, files) => {
+//   files.forEach((el) => {
+//     console.log(el);
+//   });
+// });
+
+// // // /<<<<>>>>>>>>>>{ {8.} Crud With file System
+
+// const fs = require("fs");
+// const path = require("path");
+// const dirPath = path.join(__dirname, "crud");
+
+// const filePath = `${dirPath}/apple.txt`;
+
+// // {Create File}
+// // fs.writeFileSync(filePath, "This a simple text file");
+
+// // {REad file}
+// // fs.readFile(filePath, "utf8", (err, item) => {
+// //   console.log(item);
+// // });
+
+// // {Update file and appendfile}
+
+// // fs.appendFile(filePath, " add file name is apple", (err) => {
+// //   if (!err) console.log("file is update successfully");
+// // });
+
+// // {Rename file and appendfile}
+
+// // fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+// //   if (!err) console.log("file is update successfully");
+// // });
+
+// // {Delete file}
+
+// // fs.unlinkSync(`${dirPath}/fruit.txt`);
