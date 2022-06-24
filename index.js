c7626388294578ba268087fbb070657bd0f77294
Nitish8800 <nitishfea120784@gmail.com>
2022-06-24 10:24:53 +0530

    make html page

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   console.log("Data sent by browser", req.query);
+//   res.send("Welcome " + req.query.name);
+// });
+
+// app.listen(5000);
+
+/* <>--------     Node JS in Hindi # 21 Make HTML page           -----------</> */
+
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log("Data sent by browser", req.query);
-  res.send("Welcome " + req.query.name);
-});
+const publicPath = path.join(__dirname, "public");
 
+app.use(express.static(publicPath));
 app.listen(5000);
