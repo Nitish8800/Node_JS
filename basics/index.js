c43e287a51b0cd5d4c831a3515fec3dad739393e
Nitish8800 <nitishfea120784@gmail.com>
2022-06-29 03:14:06 +0530

    mongoose start

--- basics/index.js
+++ basics/index.js
@@ -41,3 +41,49 @@
 // });
 
 app.listen(5000);
+
+
+{
+    /* <>
+    ------ Node JS in Hindi # 26 Route level Middleware express js
+    -------------------
+  </>; */
+  }
+  
+  // const express = require("express");
+  // const reqFilter = require("./middleware");
+  // const app = express();
+  // const route = express.Router();
+  
+  // // app.use(reqFilter);
+  // route.use(reqFilter);
+  
+  // app.get("/", (req, resp) => {
+  //   resp.send("Weclome to Home Page");
+  // });
+  
+  // // app.get("/users", reqFilter, (req, resp) => {
+  // //   resp.send("Weclome to Users Page");
+  // // });
+  // app.get("/users", (req, resp) => {
+  //   resp.send("Weclome to Users Page");
+  // });
+  
+  // route.get("/about", (req, resp) => {
+  //   resp.send("Weclome to About Page");
+  // });
+  
+  // route.get("/contact", (req, resp) => {
+  //   resp.send("Weclome to Contact Page");
+  // });
+  
+  // app.use("/", route);
+  
+  // app.listen(5000);
+  
+  {
+    /* <>
+    ------Node JS in Hindi # 30 Connect MongoDB with Node js -------------------
+  </>; */
+  }
+  
--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-const express = require("express");
-const reqFilter = require("./middleware");
-const app = express();
-const route = express.Router();
 
-// app.use(reqFilter);
-route.use(reqFilter);
+  /* <>
+  ------  Node JS in Hindi #39 Mongoose with node  -------------------
+</>; */
 
-app.get("/", (req, resp) => {
-  resp.send("Weclome to Home Page");
-});
 
-// app.get("/users", reqFilter, (req, resp) => {
-//   resp.send("Weclome to Users Page");
-// });
-app.get("/users", (req, resp) => {
-  resp.send("Weclome to Users Page");
-});
+const mongoose
 
-route.get("/about", (req, resp) => {
-  resp.send("Weclome to About Page");
-});
 
-route.get("/contact", (req, resp) => {
-  resp.send("Weclome to Contact Page");
-});
-
-app.use("/", route);
-
-app.listen(5000);
