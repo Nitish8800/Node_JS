ad2241ae3f376d33e04c6f446268e72c68ef03b7
Nitish8800 <nitishfea120784@gmail.com>
2022-06-30 02:12:57 +0530

    api done crud

--- index.js
+++ index.js
@@ -5,27 +5,38 @@ const app = express();
 
 connectDB();
 app.use(express.json());
-app.post("/create", async (req, resp) => {
-  let data = new Product(req.body);
-  const result = await data.save();
-  resp.send(result);
-});
 
-app.get("/list", async (req, resp) => {
-  let data = await Product.find();
-  resp.send(data);
-});
+// app.post("/create", async (req, resp) => {
+//   let data = new Product(req.body);
+//   const result = await data.save();
+//   resp.send(result);
+// });
 
-app.delete("/delete/:_id", async (req, resp) => {
-  
-  console.log(req.params);
-  let data = await Product.deleteOne(req.params);
-  resp.send(data);
-});
+// app.get("/list", async (req, resp) => {
+//   let data = await Product.find();
+//   resp.send(data);
+// });
 
-app.patch("/update/:_id", async (req, resp) => {
-  console.log(req.params);
-  let data = await Product.updateOne(req.params, { $set: req.body });
+// app.delete("/delete/:_id", async (req, resp) => {
+
+//   console.log(req.params);
+//   let data = await Product.deleteOne(req.params);
+//   resp.send(data);
+// });
+
+// app.patch("/update/:_id", async (req, resp) => {
+//   console.log(req.params);
+//   let data = await Product.updateOne(req.params, { $set: req.body });
+//   resp.send(data);
+// });
+
+app.get("/search/:key", async (req, resp) => {
+  let data = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { brand: { $regex: req.params.key } },
+    ],
+  });
   resp.send(data);
 });
 
