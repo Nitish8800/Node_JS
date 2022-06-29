632405be6d3077676c85eb1389b86ab1abb936c3
Nitish8800 <nitishfea120784@gmail.com>
2022-06-30 02:45:05 +0530

    os

--- index.js
+++ index.js
@@ -75,10 +75,11 @@
 
 // //<<<<<<<<<<<<<<<<<<<<<<<<<  Node JS in Hindi #45 OS modulee  >>>>>>>>>>>>>>>>>
 
-const os = require("os");
-console.log(os);
-// console.log(os.arch());
-// console.log(os.freemem()/(1024*1024*1024));
-// console.log(os.totalmem()/(1024*1024*1024));
-// console.log(os.platform());
-console.log(os.userInfo());
+// const os = require("os");
+// console.log(os);
+// // console.log(os.arch());
+// // console.log(os.freemem()/(1024*1024*1024));
+// // console.log(os.totalmem()/(1024*1024*1024));
+// // console.log(os.platform());
+// console.log(os.userInfo());
+
