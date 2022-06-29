5673d62676525a591fd44ecdc9764a9701335719
Nitish8800 <nitishfea120784@gmail.com>
2022-06-29 23:26:53 +0530

    mongoose start

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-
-  /* <>
+/* <>
   ------  Node JS in Hindi #39 Mongoose with node  -------------------
 </>; */
 
+const mongoose = require("mongoose");
 
-const mongoose
+const main = async () => {
+  await mongoose.connect("mongodb://localhost:27017/ecommerce");
 
+  const productSchema = new mongoose.Schema({
+    name: String,
+  });
 
+  const ProductsModel = mongoose.model("products", productSchema);
+  let data = new ProductsModel({
+    name: "Colgate Maxfresh Blue Gel Peppermint Ice Toothpaste",
+  });
+  let res = await data.save();
+
+  console.log(res);
+};
+main();
