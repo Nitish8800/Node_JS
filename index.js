7a42320f2eca569ce8e2b48397c9816516e9e1ca
Nitish8800 <nitishfea120784@gmail.com>
2022-06-30 02:04:40 +0530

    api done crud

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ app.post("/create", async (req, resp) => {
 app.get("/list", async (req, resp) => {
   let data = await Product.find();
   resp.send(data);
-  
 });
 
 app.delete("/delete/:_id", async (req, resp) => {
-
+  
   console.log(req.params);
   let data = await Product.deleteOne(req.params);
   resp.send(data);
